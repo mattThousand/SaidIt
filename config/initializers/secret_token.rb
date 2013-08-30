9172ceee08581004a16163289a088dbda819fe09
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-08-30 00:19:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaidIt::Application.config.secret_token = 'd96e6ffbee74837293f571c28ff39145e3ac1f8ec57159b5070898a07650a44f0dae8327523395bdc00e519d736267349163e3d32b97ca599bd82b15ef6beff4'
