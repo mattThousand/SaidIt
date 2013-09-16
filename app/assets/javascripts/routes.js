e9bb3a95fe95561b2869a12c57168fa6de7a2331
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-09-16 08:16:54 -0400

    Adjust default routing

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -2,9 +2,25 @@ SaidIt.Router.reopen({
   rootURL: '/'
 });
 
+SaidIt.Router.map(function() {
+  this.resource('tweeters', function() {
+    this.route('new');
+  });
+});
+
 
 SaidIt.ApplicationRoute = Ember.Route.extend({
   model: function(params) {
     return this.get('store').find('tweeter', params.tweeter_id);
+  },
+
+  redirect: function() {
+    this.transitionTo('tweeters.new');
+  }
+});
+
+SaidIt.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('tweeters.new');
   }
 });
