832d42adcb99316d9c77e2e8ca1fef2181c0f138
mattThousand <matt.d.buckley1212@gmail.com>
2013-10-04 09:00:51 -0400

    Additional integration tests, 'path' test helper

--- test/javascripts/user_visits_index_page_test.js
+++ test/javascripts/user_visits_index_page_test.js
@@ -7,9 +7,16 @@ module("/", {
   }
 });
 
+test("Initial redirect", function(){
+  expect(1);
+
+  visit("/").then(function(){
+    equal(path(), "tweeters.new", "Redirects to /tweeters/new");
+  });
+});
 
 test("/", function(){
-  expect(4);
+  expect(6);
 
   visit("/").then(function() {
     ok(exists("form"), "New tweeter form is present");
@@ -24,9 +31,20 @@ test("/", function(){
   });
 
   visit("/").then(function() {
+    equal(find("a#how-it-works-btn").length, 1, "There is a 'how it works' button");
+  });
+
+  visit("/").then(function() {
+    return click("a#how-it-works-btn").then(function(){
+      equal(find("h6").text(), "Enter any existing twitter handle to get a breakdown of their 20 most recent tweets by expressed emotion. Go ahead... try it...", 
+        "'how it works' text displays when 'how it works' button is clicked");
+    });
+  });
+
+  visit("/").then(function() {
     return fillIn(".handle", "mattthousand").then(function() {
       return click("a.form-submit").then(function() {
-        equal(1,1);
+        equal(find("#tweeter_loading_bar").length, 1, "Loading template is rendered");
       });
     });
   });
