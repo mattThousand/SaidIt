5c620d033a287a8c721988554b272724c4beacc4
mattThousand <matt.d.buckley1212@gmail.com>
2013-10-09 08:54:05 -0400

    More d3 progress, need to fix hover color

--- app/assets/javascripts/charts/barchart.js
+++ app/assets/javascripts/charts/barchart.js
@@ -14,19 +14,19 @@ SaidIt.BarChart.reopenClass({
                         pctAmbiguous) {
     var margin, data;
     data = [
-            [pctJoy, 1, "Joy: "+pctJoy, 25],
-            [pctAnger, 2, "Anger: "+pctAnger, 25],
-            [pctSadness, 3, "Sadness: "+pctSadness, 13],
-            [pctFear, 4, "Fear: "+pctFear, 29],
-            [pctSurprise, 5, "Surprise: "+pctSurprise, 17],
-            [pctDisgust, 6, "Disgust: "+pctDisgust, 18],
-            [pctAmbiguous, 7, "Ambiguous: "+pctAmbiguous, 1]
+            [pctJoy, 1, "Joy: "+pctJoy*100+"%", -60],
+            [pctAnger, 2, "Anger: "+pctAnger*100+"%", -60],
+            [pctSadness, 3, "Sadness: "+pctSadness*100+"%", -72],
+            [pctFear, 4, "Fear: "+pctFear*100+"%", -56],
+            [pctSurprise, 5, "Surprise: "+pctSurprise*100+"%", -73],
+            [pctDisgust, 6, "Disgust: "+pctDisgust*100+"%", -70],
+            [pctAmbiguous, 7, "Ambiguous: "+pctAmbiguous*100+"%", -84]
     ];
     margin = {
-      top: 0,
+      top: 30,
       right: 30,
       bottom: 30,
-      left: 0
+      left: 30
     };
 
     width = chartWidth - margin.left - margin.right;
@@ -52,7 +52,7 @@ SaidIt.BarChart.reopenClass({
 
     var chart = d3.select(chartSelector).append("svg")
                   .attr("class", "chart")
-                  .attr("width", width*data.length - 1)
+                  .attr("width", width)
                   .attr("height", height);
 
     getText = function(d) {
@@ -67,6 +67,14 @@ SaidIt.BarChart.reopenClass({
 
     getY = function(d) { return Math.max(height - Math.max((d[0]*1000), 10), 50); };
 
+    onMouseOver = function(d, i) {
+      return d3.select(this).classed('over', true);
+    };
+
+    onMouseOut = function(d, i) {
+      return d3.select(this).classed('over', false);
+    };
+
     var attrs ={
       text: {
         "fill": "#2980b9",
@@ -80,7 +88,7 @@ SaidIt.BarChart.reopenClass({
     chart.selectAll("rect")
             .data(data)
           .enter().append("rect")
-            .attr("x", function(d, i) { return (d[1]*150); })
+            .attr("x", function(d, i) { return ((d[1]-1)*150+50); })
             .attr("y", function(d) { return Math.max(height - Math.max((d[0]*1000), 10), 50); })
             .attr("width", width/8)
             .attr("height", function(d) { return (Math.max(d[0]*1000, 10))});
@@ -90,49 +98,12 @@ SaidIt.BarChart.reopenClass({
      .attr("x2", width * data.length)
      .attr("y1", height - .5)
      .attr("y2", height - .5)
-     .style("stroke", "#2980b9");
-
+     .style("stroke", "#f5f5f5");
 
     chart.selectAll(".text").data(data).enter().append("text").attr(attrs.text).text(getText);
-    
+    chart.selectAll(".rect").data(data).enter().append("rect").attr(attrs.rect).on("mouseover", onMouseOver).on("mouseout", onMouseOut);
 
-
-    // xCounter = -1;
-    // xScale = d3.scale.linear().domain(xDomain).rangeRound([0, width]);
-    // yScale = d3.scale.linear().domain(yDomain).rangeRound([0, height]);
-    // xAxis = d3.svg.axis().scale(xScale).tickValues(["Joy", "Anger", "Fear", "Surprise", "Disgust", "Sadness", "Ambiguous"]);
-    // getX = function(d) {
-    //   xCounter++;
-    //   return xCounter*120;
-    // },
-    // getY = function(d) {
-    //   return yScale(Math.max(d[0]*2, 0.01));
-    // },
-    // attrs = {
-    //   container: {
-    //     width: chartWidth,
-    //     height: chartHeight
-    //   },
-    //   graph: {
-    //     transform: "translate(" + margin.left + "," + margin.top + ")"
-    //   },
-    //   xAxis: {
-    //     "class": 'axis-x',
-    //     transform: "translate(0, 25)"
-    //   },
-    //   rect: {
-    //     "class": "rect",
-    //     "x": getX,
-    //     "y": getY,
-    //     "width": "68",
-    //     "height": getY
-    //   }
-    // };
-
-
-    // chart = d3.select(chartSelector).append("svg").attr(attrs.container).append("g").attr(attrs.graph);
-    // debugger;
-    // chart.append("g").attr(attrs.xAxis).call(xAxis);
-    // chart.selectAll(".rect").data(data).enter().append("rect").attr(attrs.rect);
+  
   }
+
 });
