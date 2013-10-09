b82ec09e54a3147252a26480f963ed399c69d136
mattThousand <matt.d.buckley1212@gmail.com>
2013-10-08 23:18:30 -0400

    Revert to 81209cc0b89ad82de793f5dbbc3d876ee586c37a

---
+++ app/assets/javascripts/charts/barchart.js
@@ -0,0 +1,113 @@
+SaidIt.BarChart = Ember.Object.extend();
+
+SaidIt.BarChart.reopenClass({
+  drawChart: function(chartSelector, 
+                        chartWidth, 
+                        chartHeight, 
+                        tweeter, 
+                        pctJoy,
+                        pctAnger,
+                        pctSadness,
+                        pctFear,
+                        pctSurprise,
+                        pctDisgust,
+                        pctAmbiguous) {
+    var margin, data;
+    data = [
+            [pctJoy, 1],
+            [pctAnger, 2],
+            [pctSadness, 3],
+            [pctFear, 4],
+            [pctSurprise, 5],
+            [pctDisgust, 6],
+            [pctAmbiguous, 7]
+    ];
+    margin = {
+      top: 0,
+      right: 30,
+      bottom: 30,
+      left: 0
+    };
+
+    width = chartWidth - margin.left - margin.right;
+    height = chartHeight - margin.top - margin.bottom;
+    xDomain = [1, data.length];
+    yDomain = [
+      0, Math.max(
+              pctJoy,
+              pctAnger,
+              pctSadness,
+              pctFear,
+              pctSurprise,
+              pctDisgust,
+              pctAmbiguous 
+            )
+    ];
+    var x = d3.scale.linear()
+              .domain(xDomain)
+              .range([0, width]);
+    var y = d3.scale.linear()
+              .domain(yDomain)
+              .rangeRound([0, height]);
+
+    var chart = d3.select(chartSelector).append("svg")
+                  .attr("class", "chart")
+                  .attr("width", width*data.length - 1)
+                  .attr("height", height);
+
+    chart.selectAll("rect")
+            .data(data)
+          .enter().append("rect")
+            .attr("x", function(d, i) { return (d[1]*150); })
+            .attr("y", function(d) { return height - Math.max((d[0]*1000), 10); })
+            .attr("width", width/8)
+            .attr("height", function(d) { return (Math.max(d[0]*1000, 10))});
+
+    chart.append("line")
+     .attr("x1", 0)
+     .attr("x2", width * data.length)
+     .attr("y1", height - .5)
+     .attr("y2", height - .5)
+     .style("stroke", "#000");
+    
+
+
+    // xCounter = -1;
+    // xScale = d3.scale.linear().domain(xDomain).rangeRound([0, width]);
+    // yScale = d3.scale.linear().domain(yDomain).rangeRound([0, height]);
+    // xAxis = d3.svg.axis().scale(xScale).tickValues(["Joy", "Anger", "Fear", "Surprise", "Disgust", "Sadness", "Ambiguous"]);
+    // getX = function(d) {
+    //   xCounter++;
+    //   return xCounter*120;
+    // },
+    // getY = function(d) {
+    //   return yScale(Math.max(d[0]*2, 0.01));
+    // },
+    // attrs = {
+    //   container: {
+    //     width: chartWidth,
+    //     height: chartHeight
+    //   },
+    //   graph: {
+    //     transform: "translate(" + margin.left + "," + margin.top + ")"
+    //   },
+    //   xAxis: {
+    //     "class": 'axis-x',
+    //     transform: "translate(0, 25)"
+    //   },
+    //   rect: {
+    //     "class": "rect",
+    //     "x": getX,
+    //     "y": getY,
+    //     "width": "68",
+    //     "height": getY
+    //   }
+    // };
+
+
+    // chart = d3.select(chartSelector).append("svg").attr(attrs.container).append("g").attr(attrs.graph);
+    // debugger;
+    // chart.append("g").attr(attrs.xAxis).call(xAxis);
+    // chart.selectAll(".rect").data(data).enter().append("rect").attr(attrs.rect);
+  }
+});
---
+++ app/assets/javascripts/views/chart_view.js
@@ -0,0 +1,24 @@
+SaidIt.chartView = Ember.View.extend({
+
+  didInsertElement: function() {
+    Ember.run.once(this, 'renderBarChart');
+  },
+
+  renderBarChart: function() {
+    if (!this.get('controller.stillLoading')) {
+      var tweeter = this.get('controller.model');
+      SaidIt.BarChart.drawChart("#sentiment_barchart",
+                        900, 
+                        365, 
+                        tweeter,
+                        this.get('controller.model.pctJoy'),
+                        this.get('controller.model.pctAnger'),
+                        this.get('controller.model.pctSadness'),
+                        this.get('controller.model.pctFear'),
+                        this.get('controller.model.pctSurprise'),
+                        this.get('controller.model.pctDisgust'),
+                        this.get('controller.model.pctAmbiguous'));
+    }
+  }
+
+});
