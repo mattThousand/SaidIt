678bbcf31f22585d457c37510f905efafbe4cd66
mattThousand <matt.d.buckley1212@gmail.com>
2013-10-14 23:01:36 -0400

    Delete tweeters_route.js

--- app/assets/javascripts/routes/tweeters_route.js
+++
@@ -1,9 +0,0 @@
-SaidIt.TweetersRoute = Ember.Route.extend({
-  model: function(params) {
-    return this.get('store').find('tweeter', params.tweeter_id);
-  },
-  setupController: function() {
-    return this.controllerFor('application').set('currentRoute', 'tweeters');
-  }
-});
-
